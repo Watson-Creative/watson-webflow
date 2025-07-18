685f350ce58c0f934ce720da07d7fa0f99fd6e1d
Spencer Thayer <spencer@watsoncreative.com>
2025-07-18 13:25:48 -0700

    Refactor custom scripts and styles for improved performance and organization. Removed unused CSS and JavaScript files, updated text animation logic, and simplified HTML structure in textanim.html.

--- js/text-animations.js
+++ js/text-animations.js
@@ -2,13 +2,24 @@
 // © Code by T.RICKS, https://www.tricksdesign.com/
 // You have the license to use this code in your projects but not redistribute it to others
 
-// Find all text with .tricks class and break each letter into a span
-var tricksWord = document.getElementsByClassName("tricks");
-for (var i = 0; i < tricksWord.length; i++) {
-  var wordWrap = tricksWord.item(i);
-  wordWrap.innerHTML = wordWrap.innerHTML.replace(/(^|<\/?[^>]+>|\s+)([^\s<]+)/g, '$1<span class="tricksword">$2</span>');
-}
+// Find all text with animation classes and break each letter into a span
+var animationClasses = ['fade-up', 'fade-up3', 'slide-up', 'slide-in', 'rotate-in', 'pop-in'];
+var elementsToAnimate = [];
 
+// Collect all elements with animation classes
+animationClasses.forEach(function(className) {
+  var elements = document.getElementsByClassName(className);
+  for (var i = 0; i < elements.length; i++) {
+    elementsToAnimate.push(elements[i]);
+  }
+});
+
+// Process each element to wrap words and letters
+elementsToAnimate.forEach(function(element) {
+  element.innerHTML = element.innerHTML.replace(/(^|<\/?[^>]+>|\s+)([^\s<]+)/g, '$1<span class="tricksword">$2</span>');
+});
+
+// Now wrap individual letters
 var tricksLetter = document.getElementsByClassName("tricksword");
 for (var i = 0; i < tricksLetter.length; i++) {
   var letterWrap = tricksLetter.item(i);
